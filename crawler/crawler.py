874f6e5927f265b9b989d220d7a5b6ce685a3abc
Francis Mutiso <franckmutiso@gmail.com>
2023-08-03 09:14:35 +0300

    Install packages and setup folder structure

---
+++ crawler/crawler.py
@@ -0,0 +1,15 @@
+class RottenTomatoesCrawler:
+    def __init__(self) -> None:
+        self.url = ""
+
+    def get_page(self):
+        pass
+
+    def get_cast(self):
+        pass
+
+    def get_reviews(self):
+        pass
+
+    def get_movie_metadata(self):
+        pass
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Entry...")
+
+
+if __name__ == "__main__":
+    main()
