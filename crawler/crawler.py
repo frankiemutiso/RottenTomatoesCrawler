0953fda42c4c7ea10b889d9863c0601fd14e866f
Francis Mutiso <franckmutiso@gmail.com>
2023-09-12 11:46:05 +0300

    print contents of cookie popup

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -280,7 +280,8 @@ class RottenTomatoesCrawler:
                 cookie_popups = self.driver.find_elements(By.ID, "onetrust-policy")
 
                 if len(cookie_popups) > 0:
-                    popup_soup = BeautifulSoup(cookie_popups, "html.parser")
+                    print(cookie_popups)
+                    popup_soup = BeautifulSoup(cookie_popups)
                     print(popup_soup)
                     cookie_popups[0].find_element(By.ID, "accept-button").click()
 
